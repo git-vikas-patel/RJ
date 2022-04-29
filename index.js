d9c9e6edf51a44ef06d4469e626d06885a98212c
Vikas Kumar Patel <86506453+git-vikas-patel@users.noreply.github.com>
2022-04-29 13:06:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+var buttons = document.querySelectorAll(".drum");
+for (var i = 0; i < buttons.length; i++)
+  buttons[i].addEventListener("click", function() {
+    var audioSource = this.innerHTML;
+    makeSound(audioSource);
+    btnAnimation(audioSource);
+  });
+
+
+document.addEventListener("keypress", function(event) {
+  makeSound(event.key);
+  btnAnimation(event.key);
+
+});
+
+function makeSound(key) {
+
+  switch (key) {
+    case 'w':
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case 'a':
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case 's':
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case 'd':
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case 'j':
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case 'k':
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case 'l':
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    default:
+      console.log("There is a Problem in Code");
+
+  }
+}
+
+function btnAnimation(currentKey) {
+  var activeBtn = document.querySelector("."+currentKey);
+  activeBtn.classList.add("pressed");
+  setTimeout(function (){
+      activeBtn.classList.remove("pressed");
+  },100)
+}
